9efcd12934139dfe9db4b2600e0ce25d0398c156
Alberto Hernandez <albertohernandezespinosa@gmail.com>
2026-05-12 19:06:05 -0600

    fix(api): export all public symbols from src/index.ts

---
+++ src/index.ts
@@ -0,0 +1,7 @@
+export type { AGUIError, AGUIMessage, AGUIToolCall, AGUIProviderProps, AGUIChatProps } from './types/index';
+export { AGUIProvider } from './components/AGUIProvider';
+export { AGUIChat } from './components/AGUIChat';
+export { useAGUIConnection } from './hooks/useAGUIConnection';
+export { useAGUIMessages } from './hooks/useAGUIMessages';
+export { useAGUIToolCalls } from './hooks/useAGUIToolCalls';
+export { useAGUISharedState } from './hooks/useAGUISharedState';
